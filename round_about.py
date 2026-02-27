5e2091e42bcf6bd984a5dd29f4baa031c89c5000
kristofer0907 <kkristjansson42@gmail.com>
2026-02-27 15:30:37 +0100

    Logic for round about task created

---
+++ round_about.py
@@ -0,0 +1,144 @@
+from uservice import service
+from spose import pose
+import time
+from simu import imu
+
+class roundAbout():
+    def __init__(self):
+        '''Constructor for the roundAbout mission'''
+        self.name = "RoundAbout"
+        self.starting_tilt = pose.pose[3]*180.0/3.14159 # store the original tilt in degrees
+        self.state = 0
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+        print(f"Current tilt: {self.starting_tilt}")
+        print()
+        print()
+        print()
+        print()
+        print()
+        print()
+                
+
+    def print_current_tilt(self):
+        '''Prints the current tilt of the robot in degrees'''
+        t = 0
+        while t < 30:
+            if service.stop:
+                break
+            print(pose.pose[3]*180.0/3.14159) # store the original tilt in degrees
+            time.sleep(0.1)
+            t += 0.1
+            
+
+    
+        
+    def get_delta_tilt(self):
+        '''Gets the tilt and returns it in degrees'''
+        tilt_old = pose.pose[3]*180.0/3.14159 #radians to degrees
+        time.sleep(1)
+        tilt_new = pose.pose[3]*180.0/3.14159 #radians to degrees
+        delta_tilt = abs(tilt_new - tilt_old)
+        return delta_tilt
+
+    def get_yaw(self):
+        return imu.gyroIntegral[2]
+
+    def execute(self):
+        """Call this repeatedly from a loop - non-blocking self.state machine"""
+        print("% RoundAbout: starting")
+        while not service.stop:
+            if self.state == 0:  # approach until tilt detected
+                service.send("robobot/cmd/ti", "rc 0.3 0.0")
+                self.state = 1
+            elif self.state == 1:  # wait until front wheel hits ramp
+                if self.get_delta_tilt() > 2:
+                    self.state = 2
+                print()
+                print(f"Current tilt: {pose.pose[3]*180.0/3.14159}")
+                print()
+                time.sleep(2)
+            elif self.state == 2:  # slow down to climb
+                service.send("robobot/cmd/ti", "rc 0.1 0.0")
+                self.state = 3
+
+            elif self.state == 3:  # wait until fully on roundabout
+                if abs(self.starting_tilt - (pose.pose[3]*180.0/3.14159)) < 3:
+                    service.send("robobot/cmd/ti", "rc 0.0 0.0")
+                    print()
+                    print(f"On roundabout tilt: {pose.pose[3]*180.0/3.14159}")
+                    print()
+                    print("% RoundAbout: on platform, starting turn")
+                    self.state = 99
+                    print("")
+                    print("")
+                    print("")
+                    print("")
+                    print("")
+                    print("")
+                    print("")
+                    print("")
+                    print("")
+                    print("")
+
+                    service.stop = True
+                
+                print(self.get_delta_tilt())
+            
+            elif self.state == 11:
+                print(11)
+                self.start_yaw = self.get_yaw()
+                self.state = 12
+                continue
+                service.send("robobot/cmd/ti", "rc 0.0 0.7") # start turning 90 degrees
+                self.state = 12
+            elif self.state == 12:
+                print(12)
+                #if self.get_yaw() >= self.start_yaw + 80:
+                if True:
+                    radius = -0.3 # radius of the roundabout in meters
+                    speed = 0.2 # speed for the roundabout in meters/second
+                    service.send("robobot/cmd/ti", f"rc {speed} {speed/radius}")
+                    self.start_yaw = self.get_yaw()
+                    self.state = 13
+            elif self.state == 13:
+                print(13)
+                if self.get_yaw() <= self.start_yaw - (360 - 45):
+                    service.send("robobot/cmd/ti", "rc 0.0 0.7")
+                    self.start_yaw = self.get_yaw()
+                    self.state = 99
+            elif self.state == 14:
+                print(14)
+                if self.get_yaw() >= self.start_yaw + 90:
+                    service.send("robobot/cmd/ti", "rc 0.0 0.0")
+                    self.state = 99
+
+            else:
+                service.send("robobot/cmd/ti", "rc 0.0 0.0")
+                print("% RoundAbout: complete")
+                break
+            #time.sleep(0.05)
+        print(f"% On roundabout, starting to turn.")
+
+
+
+
+             # in degrees, TODO: assign a more accurate threshold for tilt
+        # while not service.stop:
+            # if self.state == 0:
+            #     # approach roundabout
+            #     service.send("robobot/cmd/ti", "rc 0.2 0.0")
+            #     self.state = 1
+            # elif self.state == 1:
+            #     tilt = pose.pose[3]
+            #     if self.some_condition():
+            #         service.send("robobot/cmd/ti", "rc 0.0 0.0")
+            #         self.state = 99
+            # else:
+            #     print(f"% RoundAbout complete")
+            #     break
+            # time.sleep(0.05)
