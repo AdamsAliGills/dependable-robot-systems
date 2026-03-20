11211a23e74ff438e63993fda50db356a39ec3d7
kristofer0907 <kkristjansson42@gmail.com>
2026-03-20 16:24:34 +0100

    added mission planner from halfway challenge

--- mqtt_python/mission_planner.py
+++ mqtt_python/mission_planner.py
@@ -1,44 +1,92 @@
 from uservice import service
 from round_about import roundAbout
-from ball_in_hole import BallInHole
-from scam import cam
-from ball_in_hole import BallInHolefrom ball_in_hole import BallInHole
+from sedge import edge
+from detection_utils import wait_ramp_bottom, wait_ramp_top, wait_turn, wait_end
+import time
 
+LONG_RAMP_TILT = 8 # TODO check real value
+SHORT_RAMP_TILT = 15 # TODO check real value
+
+def sleep(t):
+    start = time.time()
+    while (time.time() - start < t) and not service.stop:
+
+        time.sleep(0.01)
 
 class missionPlanner():
     def __init__(self):
-        try: 
-            cam.setup()
-        except Exception as e:
-            print(f"Error in setting up camera: {e}")
-        try:
-            self.planMission()
-        except Exception as e:
-            print(f"Error in mission planner: {e}")
+        self.planMission()
 
     def planMission(self):
-        print(f"Mission planner is planning the mission...")
-        
+        edge.lineControl(0.2, followLeft=True)
+
+        wait_turn(25)
+        sleep(1)
+
+        edge.lineControl(0.05, followLeft=True)
+
+        r = roundAbout(-225)
+        r.execute()
+
+        edge.lineControl(0.2, followLeft=False)
+        sleep(3)
+
+        wait_ramp_bottom(LONG_RAMP_TILT, tolerance = 3)
+        print(f"START FIRST RAMP")
+        sleep(1)
+        edge.lineControl(0.3, followLeft=False)
+
+        wait_ramp_top(LONG_RAMP_TILT, tolerance = 3)
+        print(f"END FIRST RAMP")
+        edge.lineControl(0.2, followLeft=False)
+
+        wait_ramp_top(SHORT_RAMP_TILT, tolerance = 3)
+        print(f"START SECOND RAMP")
+        edge.lineControl(0.25, followLeft=False)
+
+        wait_ramp_bottom(SHORT_RAMP_TILT, tolerance = 3)
+        print(f"END SECOND RAMP")
+        edge.lineControl(0.15, followLeft=True)
+
+        wait_turn(80)
+        print(f"TURN DETECTED")
+
+        sleep(1.5)
+        edge.lineControl(0)
+        service.send("robobot/cmd/ti", f"rc 0.2 0.0")
+        sleep(1)
+
+        edge.lineControl(0.2, followLeft=True)
+
+        sleep(3.5)
+
+        edge.lineControl(0.05, followLeft=True)
+
+        r = roundAbout(-225)
+        r.execute()
+
+        edge.lineControl(0.2, followLeft=True)
+        wait_end()
+
+
+
+
+
     def roundAboutCaller(self):
         '''Caller for the roundabout mission'''
-        r = roundAbout(135)
-        r.execute()
+        tot_tests = 10
+        n = roundAbout.test(tot_tests)
+        print()
+        print(f"===================== TESTS RESULTS =====================")
+        print(f"{n} tests out of {tot_tests} were successful")
+        print(f"=========================================================")
+        print()
+
     
     def seeSawCaller(self):
         '''Caller for the see saw mission'''
         pass
 
-    def ballInHoleCaller(self,image):
-    def ballInHoleCaller(self,image):
+    def ballInHoleCaller(self):
         '''Caller for the ball in hole mission'''
-        print("---------------------------")
-        print("Executing image analysis...")
-        print("---------------------------")
-        BallInHole()
-        
-        print("---------------------------")
-        print("Executing image analysis...")
-        print("---------------------------")
-        BallInHole()
-        
-
+        pass
