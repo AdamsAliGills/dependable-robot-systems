1654db2a41cc72860249de8cff1e3aa3b3e9c5f2
k1tten0m0 <hehaochen2002@163.com>
2026-04-16 21:22:48 +0200

    guard robot

--- mqtt_python/guard_robot.py
+++ mqtt_python/guard_robot.py
@@ -13,12 +13,13 @@ from sedge import edge
 from sgpio import gpio
 from scam import cam
 from uservice import service
+from simu import imu
 
 class GuardRobot: 
     
     ENTRANCE_DISTANCE = 0.3  #when  TODO check the real value 
     MIN_DISTANCE = 0.2
-    MISSION_DISTANCE = 1.6 #TODO check the real value
+    MISSION_DISTANCE = 1.8 #TODO check the real value
 
     def __init__(self):
         self.state = 0
@@ -42,6 +43,8 @@ class GuardRobot:
                     self.state = 2
 
             elif self.state == 2:
+                #dis = ir.ir[1]
+                #print(f"Distance between Oscar and Guard robot: {dis} \n")
                 if ir.ir[1] < self.MIN_DISTANCE: # Oscar is too close to the guard robot
                     edge.lineControl(0)
                     service.send("robobot/cmd/ti","rc 0.0 0.0")
@@ -50,7 +53,7 @@ class GuardRobot:
                     #service.send("robobot/cmd/ti","rc 0.2 0.0")
 
                 
-                if pose.tripB > self.MISSION_DISTANCE: #Mission is finished
+                if pose.tripB > self.MISSION_DISTANCE: #Already pass two gates
                     edge.lineControl(0)
                     pose.tripBreset()
                     service.send("robobot/cmd/ti","rc 0.2 0.0")
@@ -59,7 +62,27 @@ class GuardRobot:
             elif self.state == 3:
                 if pose.tripB > 0.2:
                     service.send("robobot/cmd/ti","rc 0.0 0.0")
+                    t.sleep(0.1)
+                    self.start_yaw = imu.gyroIntegral[2]
+                    service.send("robobot/cmd/ti","rc 0.0 1.0") # Turn around
+                    self.state = 4
+                    #self.state = 99
+
+            elif self.state == 4: #turn around the robot and prepare for going back to the ramp
+                current_yaw = imu.gyroIntegral[2] - self.start_yaw
+                if abs(current_yaw) >= 170: 
+                    service.send("robobot/cmd/ti","rc 0.0 0.0")
+                    t.sleep(0.1)
+                    pose.tripBreset()
+                    service.send("robobot/cmd/ti","rc 0.4 0.0")
+                    self.state = 5
+
+            elif self.state == 5:
+                if pose.tripB > 0.7:
+                    service.send("robobot/cmd/ti","rc 0.0 0.0")
                     self.state = 99
+
+
 
             else:
                 print("GuardRobot: complete")
