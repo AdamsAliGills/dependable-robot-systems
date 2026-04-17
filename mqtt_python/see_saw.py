95b5b607567a22f0be62689e1e84279c23fa08e9
k1tten0m0 <hehaochen2002@163.com>
2026-04-17 15:23:36 +0200

    see saw

--- mqtt_python/see_saw.py
+++ mqtt_python/see_saw.py
@@ -6,9 +6,6 @@ from datetime import *
 import time
 import math
 
-MIDDLE_DISTANCE = 0.85 # TODO check real value
-BOTTOM_DISTANCE = 1.0 # TODO check real value
-
 def get_yaw():
     return imu.gyroIntegral[2]
 
@@ -19,6 +16,12 @@ def stateTimePassed():
   return (datetime.now() - stateTime).total_seconds()
 
 class SeeSaw():
+
+    
+    MIDDLE_DISTANCE = 0.85 # TODO check real value
+    BOTTOM_DISTANCE = 1.0 # TODO check real value
+
+
     def __init__(self):
         '''Constructor for the SeeSaw mission'''
         self.name = "SeeSaw"
@@ -46,7 +49,7 @@ class SeeSaw():
             elif self.state == 2:
                 '''Move to the middle and pick up the ball'''
                 print(f"distance: {pose.tripB}")
-                if pose.tripB > MIDDLE_DISTANCE:     # pose.tripB is the variable to record distance
+                if pose.tripB > self.MIDDLE_DISTANCE:     # pose.tripB is the variable to record distance
                     edge.lineControl(0)
                     service.send("robobot/cmd/ti","rc 0.0 0.0")
                     time.sleep(5) # TODO Control the servo to pick up the golf ball
@@ -69,7 +72,7 @@ class SeeSaw():
             elif self.state == 4:
                 print(f"distance: {pose.tripB}")
                
-                if pose.tripB > BOTTOM_DISTANCE: # When robot leave the see-saw
+                if pose.tripB > self.BOTTOM_DISTANCE: # When robot leave the see-saw
                     edge.lineControl(0)
                     self.state = 99
 
