f37b7fc75c22550d77452f2df19aecdd93e80c3f
k1tten0m0 <hehaochen2002@163.com>
2026-04-23 13:01:24 +0200

    see_saw

--- mqtt_python/see_saw.py
+++ mqtt_python/see_saw.py
@@ -5,6 +5,7 @@ from simu import imu
 from datetime import *
 import time
 import math
+from ball_in_hole import BallInHole
 
 def get_yaw():
     return imu.gyroIntegral[2]
@@ -14,6 +15,8 @@ stateTime = datetime.now()
 
 def stateTimePassed():
   return (datetime.now() - stateTime).total_seconds()
+
+pickup = BallInHole()
 
 class SeeSaw():
 
@@ -41,7 +44,7 @@ class SeeSaw():
             elif self.state == 1:
                 current_yaw = get_yaw()
                 print(f"yaw: {current_yaw}")
-                if abs(current_yaw-self.starting_yaw) >= 75: #detect the turning - reach the see-saw
+                if abs(current_yaw-self.starting_yaw) >= 75: #detect the turning -- reach the see-saw
                     pose.tripBreset()  #reset the distance record
                     edge.lineControl(0.15, followLeft=True) # slow down the speed
                     self.state = 2
@@ -52,24 +55,39 @@ class SeeSaw():
                 if pose.tripB > self.MIDDLE_DISTANCE:     # pose.tripB is the variable to record distance
                     edge.lineControl(0)
                     service.send("robobot/cmd/ti","rc 0.0 0.0")
-                    time.sleep(5) # TODO Control the servo to pick up the golf ball
-                    #
-                    #
-                    # Pick up function
-                    #
-                    #
-                    pose.tripBreset()
-                    edge.lineControl(0.04, followLeft=True)
                     self.state = 3  
 
             elif self.state == 3:
+                reached = pickup._approaching()
+                if reached:
+                    service.send("robobot/cmd/ti", "rc 0 0")
+                    if self.final_alignment == False:
+                        print("#####################")
+                        print("CHECKING FINAL ALLIGNMENT")
+                        print("#####################")
+                        self.state = self.ALIGNING
+                        self.final_alignment = True
+                    self.state = 4
+                    print(f"[BallInHole] Ball in reach, transitioning to PICKING_UP")
+
+            elif self.state == 4:
+                pickup._picking_up()
+                print(f"[BallInHole] Ball picked up, transitioning to NAVIGATING_HOLE")
+                self.state = 5
+
+            elif self.state == 5:
+                pose.tripBreset()
+                edge.lineControl(0.04, followLeft=True)
+                self.state = 11 
+
+            elif self.state == 11:
                 if pose.tripBtimePassed() > 7:
                     pose.tripBreset()
                     edge.lineControl(0.10, followLeft=True)
-                    self.state = 4 
+                    self.state = 12 
             
             
-            elif self.state == 4:
+            elif self.state == 12:
                 print(f"distance: {pose.tripB}")
                
                 if pose.tripB > self.BOTTOM_DISTANCE: # When robot leave the see-saw
